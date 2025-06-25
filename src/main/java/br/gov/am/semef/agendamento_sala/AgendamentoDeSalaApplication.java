2e580e08c0d456f6f7a01e0051f9011c4bd811bc
Manoel Lima <manoelsslima@yahoo.com.br>
2025-06-25 19:17:09 -0400

    feat: projeto base

---
+++ src/main/java/br/gov/am/semef/agendamento_sala/AgendamentoDeSalaApplication.java
@@ -0,0 +1,13 @@
+package br.gov.am.semef.agendamento_sala;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendamentoDeSalaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendamentoDeSalaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/gov/am/semef/agendamento_sala/AgendamentoDeSalaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.gov.am.semef.agendamento_sala;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendamentoDeSalaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
