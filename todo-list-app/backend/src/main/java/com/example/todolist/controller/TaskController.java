238566caa20f386a15c617d13f71bf9a1f638334
adrinebt <adrinebrandao@gmaail.com>
2025-06-25 20:53:17 -0400

    [feature] Adicionada funcionalidade de filtro por prioridade

--- todo-list-app/backend/src/main/java/com/example/todolist/controller/TaskController.java
+++ todo-list-app/backend/src/main/java/com/example/todolist/controller/TaskController.java
@@ -1,5 +1,4 @@
 package com.example.todolist.controller;
-
 import com.example.todolist.model.Task;
 import com.example.todolist.service.TaskService;
 import org.springframework.beans.factory.annotation.Autowired;
