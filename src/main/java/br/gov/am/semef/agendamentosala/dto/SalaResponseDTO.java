dde2b5bc4df620363177f25003c1acfa44a2116c
Manoel Lima <manoelsslima@yahoo.com.br>
2025-06-26 18:42:27 -0400

    feat: add DTO classes for HorarioDisponivel, Reserva, and Sala

---
+++ src/main/java/br/gov/am/semef/agendamentosala/dto/SalaResponseDTO.java
@@ -0,0 +1,18 @@
+package br.gov.am.semef.agendamentosala.dto;
+
+public class SalaResponseDTO {
+    private Long id;
+    private String nome;
+
+    public SalaResponseDTO() {}
+
+    public SalaResponseDTO(Long id, String nome) {
+        this.id = id;
+        this.nome = nome;
+    }
+
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+    public String getNome() { return nome; }
+    public void setNome(String nome) { this.nome = nome; }
+}
