cff38d86329569109e4efc45823ba308fe86df93
Jarlan Castro da Costa <jarlancastro@gmail.com>
2025-06-25 21:11:00 -0400

    Primeiro Commit

---
+++ jarlan-agenda/src/main/java/com/agenda/contatos/repository/ContatoRepository.java
@@ -0,0 +1,5 @@
+package com.agenda.contatos.repository;
+
+public interface ContatoRepository {
+    
+}
---
+++ jarlan-agenda/src/main/java/com/example/jarlanagenda/JarlanAgendaApplication.java
@@ -0,0 +1,11 @@
+package com.example.jarlanagenda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JarlanAgendaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(JarlanAgendaApplication.class, args);
+    }
+}
